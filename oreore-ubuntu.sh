f2d3a7152910cae0b80b7f1c4370cff4281d39c1
Taiki-ss <81791711+Taiki-ss@users.noreply.github.com>
2022-09-08 23:36:28 +0900

    Brave install

--- oreore-ubuntu.sh
+++ oreore-ubuntu.sh
@@ -101,6 +101,13 @@ sudo snap install slack --classic
 # Qlipper
 sudo apt install -y qlipper
 
+# Brave
+sudo apt install apt-transport-https curl
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
+
 # CapsLock to Ctrl
 gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
 

3c68fd845ea24202876e4debf991ea96bfe4bc4f
Taiki-ss <81791711+Taiki-ss@users.noreply.github.com>
2022-09-23 23:25:41 +0900

    vim-plug追加

--- oreore-ubuntu.sh
+++ oreore-ubuntu.sh
@@ -54,6 +54,10 @@ password $github_token" > ~/.netrc
 # vim
 sudo apt install vim neovim -y
 
+# vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 # termiantor
 sudo apt install terminator
 
